270e074a60cff84792e67df3f817b5f495394e3b
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2022-03-21 18:13:38 +0530

    Update SearchBox.jsx

--- src/components/search/SearchBox.jsx
+++ src/components/search/SearchBox.jsx
@@ -5,7 +5,6 @@ const SearchBox = (props) => {
     <div className="container flex mx-auto">
       <div className="flex border-2 rounded">
         <input
-          defaultValue="admin"
           type="text"
           className="px-4 py-2 w-80"
           placeholder="Search..."
